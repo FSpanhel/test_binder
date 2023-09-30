384e0268f2181b5324b6073c38263f65ec9ab3b9
Fabian Spanhel <fabian.spanhel@prosiebensat1.com>
2023-09-30 16:13:26 +0200

    Add emails of 2023

--- _organization/students/put_emails_into_csv.py
+++ _organization/students/put_emails_into_csv.py
@@ -1,4 +1,5 @@
 """
+If run from the folder of this script,
 `python3 put_emails_into_csv students_Data_Science_Challenge.csv`
 extracts the emails of `students_Data_Science_Challenge.csv` and puts
 them into `email.csv`.
@@ -12,7 +13,7 @@ if __name__ == "__main__":
     output_path = os.path.join(os.path.dirname(sys.argv[1]), "email.csv")
     (
         pd.read_csv(sys.argv[1], delimiter=";")
-        .filter("Email")
+        .filter(["Email"])
         .to_csv(output_path, index=False, header=False)
     )
     print(f"> {output_path} created")
